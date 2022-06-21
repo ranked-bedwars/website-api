905e97e439c836f47c7ceebe5416599a8cd5c095
cake <thecactivecake@gmail.com>
2022-06-21 20:50:00 +1000

    Better Handled Errors

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,27 @@ use serde::{Deserialize, Serialize};
 use actix_web::{get, rt, web, App, HttpServer, Responder};
 
 #[derive(Serialize, Deserialize, Clone)]
-struct Profile {
+struct LeaderboardProfile {
     rating: u32,
 }
 
 #[derive(Serialize, Deserialize, Clone)]
-struct Player {
+struct LeaderboardPlayer {
     discord_id: String,
     username: String,
     uuid: String,
-    profile: Profile,
+    profile: LeaderboardProfile,
     position: u32,
 }
 
 #[derive(Clone)]
 struct Manager {
     last_updated: Instant,
-    players: Vec<Player>,
+    players: Vec<LeaderboardPlayer>,
 }
 
-fn leaderboard_players() -> Vec<Player> {
-    (0..10)
-        .into_iter()
-        .map(|i| fetch_data(i * 10).unwrap())
-        .flatten()
-        .collect()
+fn leaderboard_players() -> Vec<LeaderboardPlayer> {
+    (0..10).into_iter().map(|i| fetch_leaderboard_segment(i * 10)).flatten().collect()
 }
 
 impl Manager {
@@ -38,42 +34,38 @@ impl Manager {
             players: Vec::new(),
         }
     }
-
     fn update_data(&mut self) {
         self.players = leaderboard_players();
         self.last_updated = Instant::now();
     }
-
-    fn get_data(&mut self) -> Vec<Player> {
+    fn get_data(&mut self) -> Vec<LeaderboardPlayer> {
         if self.last_updated.elapsed().as_secs() > 60 * 5 {
             self.update_data();
         }
-
         self.players.clone()
     }
 }
-
-fn fetch_data(skip: u32) -> Result<Vec<Player>, String> {
+fn fetch_leaderboard_segment(skip: u32) -> Vec<LeaderboardPlayer> {
     let request = match reqwest::blocking::get(format!(
         "https://api.rankedbedwars.org/leaderboard/ranked/rating?skip={}&ascending=false&limit=10",
         skip.to_owned()
     )) {
         Ok(req) => req,
-        Err(e) => return Err(e.to_string()),
+        Err(_) => return Vec::new(),
     };
 
-    let players: Vec<Player> = match request.json::<Vec<Player>>() {
+    let players: Vec<LeaderboardPlayer> = match request.json::<Vec<LeaderboardPlayer>>() {
         Ok(players) => players,
-        Err(e) => return Err(e.to_string()),
+        Err(_) => return Vec::new(),
     };
 
-    Ok(players)
+    players
 }
 
 #[derive(Serialize, Deserialize)]
 struct Response {
     success: bool,
-    data: Vec<Player>,
+    data: Vec<LeaderboardPlayer>,
 }
 
 #[get("/leaderboard")]
